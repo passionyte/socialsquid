054dbe280500bcb7494c2d8cfea4beeb992d7556
LoganValenzuela <logan.valenzuela@stu.ocsb.ca>
2024-10-03 11:11:26 -0400

    Video will only load if views are above 0

--- scripts/game.js
+++ scripts/game.js
@@ -176,7 +176,7 @@ function load() { // Convert our saved data to a expendible format.. load from l
 
             setTimeout(ready, (cd * 1000))
         }
-        if (data.video) {
+        if (data.video.views > 0) {
             videoui.hidden = false
             vidtitletext.innerText = data.video.title
             thumb.innerText = data.video.title
