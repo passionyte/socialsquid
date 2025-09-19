76492b8dbc50d4960fc6ce3b3fb5a2ef12d10842
passionyte <logan.valenz07@gmail.com>
2025-09-18 23:38:38 -0400

    Some rebalancing, timescale changed to 2 cause this game is boring

--- scripts/game.js
+++ scripts/game.js
@@ -78,48 +78,50 @@ let upgvars = {
     "viewmult": 1,
     "likemult": 1,
     "dislikemult": 1,
-    "cooldown": 120,
-    "vidlifetime": 60,
+    "cooldown": 60,
+    "vidlifetime": 30,
     "viralchance": 0,
     "buttonboost": 1
 }
 
 // GAME
-let timescale = 1
+let timescale = 2
 let fps = 30
 let loaded = false
-const vers = "0.02_1 alpha"
+const vers = "0.02_2 alpha"
 
 let interval
 
 const upgrades = [
     // Subscriber Progression
-    {Name: "Amateur Videos", Cost: 100, Description: "Well, you gotta start somewhere. (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true},
+    {Name: "Amateur Videos", Cost: 50, Description: "Well, you gotta start somewhere. (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true},
     {Name: "Share With Friends", Cost: 100, Description: "Your friends are sure to check out your videos! (+10% views)", Stat: "viewmult", Inc: 0.1, Available: true},
-    {Name: "Subscriber Begging", Cost: 500, Description: "Begging for subscribers raises the chance you get some! (+5% chance for subs)", Stat: "subchance", Inc: 0.05, Available: true},
-    {Name: "Faster Wi-Fi", Cost: 500, Description: "Wi-Fi will time out less if you upgrade it to be faster. (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true},
-    {Name: "Smash Like", Cost: 1000, Description: "Asking your viewers to smash the like button will increase your likes! (+5% chance for likes)", Stat: "likechance", Inc: 0.05, Available: true},
+    {Name: "Subscriber Begging", Cost: 300, Description: "Begging for subscribers raises the chance you get some! (+5% chance for subs)", Stat: "subchance", Inc: 0.05, Available: true},
+    {Name: "Sub4Sub", Cost: 500, Description: "Now you're desperate... (+5% subs)", Stat: "submult", Inc: 0.05, Available: true},
+    {Name: "Faster Wi-Fi", Cost: 400, Description: "Wi-Fi will time out less if you upgrade it to be faster. (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true},
+    {Name: "Smash Like", Cost: 800, Description: "Asking your viewers to smash the like button will increase your likes! (+15% chance for likes)", Stat: "likechance", Inc: 0.15, Available: true},
     {Name: "Smarter Titles", Cost: 1000, Description: "Better titles will make more people click. (Guaranteed chance for views)", Stat: "viewchance", Inc: 0.1, Available: true},
-    {Name: "Experimentation", Cost: 2500, Description: "Having experimentation will draw more recommendations from the algorithm. (+10% views)", Stat: "viewmult", Inc: 0.1, Available: true, Requirements: {subs: 100}},
-    {Name: "Enthusiasm", Cost: 2500, Description: "Perhaps not being boring may help your watch time! (+10s video lifetime)", Stat: "vidlifetime", Inc: 10, Available: true, Requirements: {subs: 100}},
-    {Name: "Commentary", Cost: 5000, Description: "Adding commentary to your videos will make them more engaging! (+5% chance for subs)", Stat: "subchance", Inc: 0.05, Available: true, Requirements: {subs: 500}},
-    {Name: "Giveaways", Cost: 5000, Description: "Say you are doing a giveaway, people are sure to like... (+5% chance for likes)", Stat: "likechance", Inc: 0.05, Available: true, Requirements: {subs: 500}},
-    {Name: "Trending Topic", Cost: 10000, Description: "Making videos on trending topics will increase the longevity. (+15s vid lifetime)", Stat: "vidlifetime", Inc: 15, Available: true, Requirements: {subs: 1000}},
-    {Name: "Webcam", Cost: 10000, Description: "Use a webcam for better viewer engagement. (+5% chance for subs)", Stat: "subchance", Inc: 0.05, Available: true, Requirements: {subs: 1000}},
+    {Name: "Experimentation", Cost: 2500, Description: "Having experimentation will draw more recommendations from the algorithm. (+15% views)", Stat: "viewmult", Inc: 0.15, Available: true, Requirements: {subs: 100}},
+    {Name: "Enthusiasm", Cost: 2000, Description: "Perhaps not being boring may help your watch time! (+10s video lifetime)", Stat: "vidlifetime", Inc: 10, Available: true, Requirements: {subs: 100}},
+    {Name: "Likeability", Cost: 3000, Description: "Make sure your audience loves you. (+10% subs)", Stat: "submult", Inc: 0.1, Available: true, Requirements: {subs: 100}},
+    {Name: "Commentary", Cost: 4000, Description: "Adding commentary to your videos will make them more engaging! (+5% chance for subs)", Stat: "subchance", Inc: 0.05, Available: true, Requirements: {subs: 500}},
+    {Name: "Giveaways", Cost: 5000, Description: "Say you are doing a giveaway, people are sure to like... (+10% chance for likes)", Stat: "likechance", Inc: 0.1, Available: true, Requirements: {subs: 500}},
+    {Name: "Trending Topic", Cost: 8000, Description: "Making videos on trending topics will increase the longevity. (+15s vid lifetime)", Stat: "vidlifetime", Inc: 15, Available: true, Requirements: {subs: 1000}},
+    {Name: "Webcam", Cost: 9000, Description: "Use a webcam for better viewer engagement. (+10% chance for subs)", Stat: "subchance", Inc: 0.1, Available: true, Requirements: {subs: 1000}},
     {Name: "Hit the bell", Cost: 10000, Description: "Tell viewers to hit the notification bell, makes them return more often! (+15% views)", Stat: "viewmult", Inc: 0.15, Available: true, Requirements: {subs: 1000}},
-    {Name: "Decent Videos", Cost: 25000, Description: "Seems like you've gotten more experienced! But, you gotta pay.. for some reason (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true, Requirements: {subs: 2000}},
-    {Name: "HD Video", Cost: 50000, Description: "Upgrade your device for better quality so less people dislike! (-5% chance for dislikes)", Stat: "dislikechance", Inc: -0.05, Available: true, Requirements: {subs: 5000}},
+    {Name: "Decent Videos", Cost: 20000, Description: "Seems like you've gotten more experienced! But, you gotta pay.. for some reason (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true, Requirements: {subs: 2000}},
+    {Name: "HD Video", Cost: 40000, Description: "Upgrade your device for better quality so less people dislike! (-10% chance for dislikes)", Stat: "dislikechance", Inc: -0.1, Available: true, Requirements: {subs: 5000}},
     {Name: "Even-Faster Wi-Fi", Cost: 50000, Description: "Reduce Wi-Fi timeout by making it EVEN-FASTER! (-15s cooldown)", Stat: "cooldown", Inc: -15, Available: true, Requirements: {subs: 5000}},
     {Name: "Custom Thumbnails", Cost: 100000, Description: "Begin creating custom thumbnails people will actually click! (+20% views)", Stat: "viewmult", Inc: 0.2, Available: true, Requirements: {subs: 10000}},
-    {Name: "Unique Content", Cost: 100000, Description: "Having more unique content will make your videos stand out from others. (+20s vid lifetime)", Stat: "vidlifetime", Inc: 20, Available: true, Requirements: {subs: 10000}},
-    {Name: "Fiber Wi-Fi", Cost: 500000, Description: "Upgrade your even-faster Wi-Fi to Fiber for even faster uploads. (-20s cooldown)", Stat: "cooldown", Inc: -20, Available: true, Requirements: {subs: 50000}},
-    {Name: "Favorability", Cost: 1000000, Description: "You've grown to the point people love you! Just believe! (+8% chance for likes)", Stat: "likechance", Inc: 0.08, Available: true, Requirements: {subs: 100000}},
-    {Name: "Sponsorships", Cost: 1000000, Description: "You get some sponsors which help you produce longer and more engaging videos. (+15s vid lifetime)", Stat: "vidlifetime", Inc: 15, Available: true, Requirements: {subs: 100000}},
-    {Name: "Collaboration", Cost: 2000000, Description: "Collaborate with other creators! (+25% views)", Stat: "viewmult", Inc: 0.25, Available: true, Requirements: {subs: 500000}},
-    {Name: "Longevity", Cost: 3000000, Description: "Looooongevity! (+15s vid lifetime)", Stat: "vidlifetime", Inc: 15, Available: true, Requirements: {subs: 1000000}},
-    {Name: "More Subs", Cost: 3000000, Description: "Subs. Subs. And... More Subs... Not sandwiches right? (+5% subs)", Stat: "submult", Inc: 0.05, Available: true, Requirements: {subs: 1000000}},
-    {Name: "Directly Sourced Fiber", Cost: 5000000, Description: "Directly source your Fiber Wi-Fi from your ISP. Expensive but lightning fast. (-20s cooldown)", Stat: "cooldown", Inc: -20, Available: true, Requirements: {subs: 1000000}},
-    {Name: "Professional Videos", Cost: 5000000, Description: "Hey now, you're an all-star.. now gimme your views. (-10s cooldown)", Stat: "cooldown", Inc: -10, Available: true, Requirements: {subs: 1000000}},
+    {Name: "Unique Content", Cost: 80000, Description: "Having more unique content will make your videos stand out from others. (+20s vid lifetime)", Stat: "vidlifetime", Inc: 20, Available: true, Requirements: {subs: 10000}},
+    {Name: "Shoutout", Cost: 110000, Description: "Yay a shoutout! (+15% subs)", Stat: "submult", Inc: 0.15, Available: true, Requirements: {subs: 10000}},
+    {Name: "Fiber Wi-Fi", Cost: 350000, Description: "Upgrade your even-faster Wi-Fi to Fiber for even faster uploads. (-20s cooldown)", Stat: "cooldown", Inc: -20, Available: true, Requirements: {subs: 50000}},
+    {Name: "Subaggedon", Cost: 400000, Description: "SUPA SUBS (+10% chance for subs)", Stat: "subchance", Inc: 0.1, Available: true, Requirements: {subs: 50000}},
+    {Name: "Favorability", Cost: 700000, Description: "You've grown to the point people love you! Just believe! (+8% chance for likes)", Stat: "likechance", Inc: 0.08, Available: true, Requirements: {subs: 100000}},
+    {Name: "Sponsorships", Cost: 800000, Description: "You get some sponsors which help you produce longer and more engaging videos. (+15s vid lifetime)", Stat: "vidlifetime", Inc: 15, Available: true, Requirements: {subs: 100000}},
+    {Name: "Collaboration", Cost: 1600000, Description: "Collaborate with other creators! (+20% views)", Stat: "viewmult", Inc: 0.2, Available: true, Requirements: {subs: 500000}},
+    {Name: "Longevity", Cost: 2200000, Description: "Looooongevity! (+15s vid lifetime)", Stat: "vidlifetime", Inc: 15, Available: true, Requirements: {subs: 1000000}},
+    {Name: "More Subs", Cost: 2800000, Description: "Subs. Subs. And... More Subs... Not sandwiches right? (+15% subs)", Stat: "submult", Inc: 0.15, Available: true, Requirements: {subs: 1000000}},
     // Niche
     {Name: "Trendy Squid is Trendy", Cost: 777777, Description: "Just be trendy™ (+10% viral chance)", Stat: "viralchance", Inc: 0.1, Available: true, Requirements: {timesviral: 7}},
 ]
@@ -236,21 +238,21 @@ function tick() {
     }
     if (stats.video.life > 0) {
         if (Math.random() <= (0.9 + upgvars.viewchance)) {
-            const inc = (((Math.floor((((stats.subs * Math.random()) / 48) + ((stats.video.likes * Math.random()) / 8))) + 1) * (((upgvars.viewmult * upgvars.buttonboost) * ((stats.video.viral) && 7) || 1))) / 60)
+            const inc = (((Math.floor((((stats.subs * Math.random()) / 48) + ((stats.video.likes * Math.random()) / 8))) + 1) * (((upgvars.viewmult * upgvars.buttonboost) * ((stats.video.viral) && 7) || 1))) / 60) * timescale
             stats.video.views += inc
             stats.views += inc
             stats.totalviews += inc
         }
-        if (Math.random() <= (0.1 + upgvars.subchance)) {
-            stats.subs += (Math.floor((((((stats.video.views + stats.subs) * Math.random()) / 128)) + 1) * (upgvars.submult * upgvars.buttonboost)) / 60)
+        if (Math.random() <= (0.2 + upgvars.subchance)) {
+            stats.subs += (Math.floor((((((stats.video.views + stats.subs) * Math.random()) / 128)) + 1) * (upgvars.submult * upgvars.buttonboost)) / 60) * timescale
         }
-        if (Math.random () <= (0.15 + upgvars.likechance)) {
-            stats.video.likes += (((Math.floor(((stats.video.views + stats.subs) * Math.random()) / 600) + 1) * upgvars.likemult) / 60)
+        if (Math.random () <= (0.25 + upgvars.likechance)) {
+            stats.video.likes += (((Math.floor(((stats.video.views + stats.subs) * Math.random()) / 600) + 1) * upgvars.likemult) / 60) * timescale
         }
-        else if (Math.random() <= (0.2 + upgvars.dislikechance)) {
-            stats.video.dislikes += (((Math.floor((stats.video.views * Math.random()) / 600) + 1) * upgvars.dislikemult) / 60)
+        else if (Math.random() <= (0.3 + upgvars.dislikechance)) {
+            stats.video.dislikes += (((Math.floor((stats.video.views * Math.random()) / 600) + 1) * upgvars.dislikemult) / 60) * timescale
         }
-        stats.video.life -= (1 / (fps * timescale))
+        stats.video.life -= ((1 / fps) * timescale)
     }  
     step()
 }
